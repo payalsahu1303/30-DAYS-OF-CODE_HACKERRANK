8f8cd6a4257e17e43bdafadd943058d784eb7d5f
Payal Sahu <141853271+payalsahu1303@users.noreply.github.com>
2025-01-08 08:00:06 +0530

    Add files via upload

---
+++ Day 24/Solution.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+class Node{
+	int data;
+	Node next;
+	Node(int d){
+        data=d;
+        next=null;
+    }
+	
+}
+class Solution
+{
+
+    public static Node removeDuplicates(Node head) {
+      //Write your code here
+      Node current1 = head;
+      while(current1 != null){
+        Node current2 = current1;
+        while(current2.next != null){
+            if(current2.next.data == current1.data){
+                current2.next = current2.next.next;
+            } else {
+                current2 = current2.next;
+            }
+        }
+        current1 = current1.next;
+      }
+      return head;
+
+    }
+
+	 public static  Node insert(Node head,int data)
+     {
+        Node p=new Node(data);			
+        if(head==null)
+            head=p;
+        else if(head.next==null)
+            head.next=p;
+        else
+        {
+            Node start=head;
+            while(start.next!=null)
+                start=start.next;
+            start.next=p;
+
+        }
+        return head;
+    }
+    public static void display(Node head)
+        {
+              Node start=head;
+              while(start!=null)
+              {
+                  System.out.print(start.data+" ");
+                  start=start.next;
+              }
+        }
+        public static void main(String args[])
+        {
+              Scanner sc=new Scanner(System.in);
+              Node head=null;
+              int T=sc.nextInt();
+              while(T-->0){
+                  int ele=sc.nextInt();
+                  head=insert(head,ele);
+              }
+              head=removeDuplicates(head);
+              display(head);
+
+       }
+    }
